447abc5a8ffc7d4e6cbf32dc7e768a5e4c182313
Tien Duc <ductiennguyenta@gmail.com>
2025-06-21 19:07:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 alert("blablablablobloblo");
+const btn = document.getElementById('toggle-music');
+const audio = document.getElementById('bg-music');
+let isPlaying = false;
+
+btn.addEventListener('click', () => {
+  if (isPlaying) {
+    audio.pause();
+    btn.textContent = '🔈';
+  } else {
+    audio.play();
+    btn.textContent = '🔊';
+  }
+  isPlaying = !isPlaying;
+});
 
