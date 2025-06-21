4f8d285e7cee7179df52183efa88c09f69709828
Tien Duc <ductiennguyenta@gmail.com>
2025-06-21 20:02:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+function updateClock() {
+  const now = new Date();
+  const date = now.toLocaleDateString("vi-VN", {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  const time = now.toLocaleTimeString("vi-VN", {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+
+  document.getElementById("date").textContent = date;
+  document.getElementById("clock").textContent = time;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); 
